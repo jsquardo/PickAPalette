371b194576e7428f9d71fbae08cecca184cb5024
Johnny Squardo <squ@rdo.host>
2019-12-01 13:47:07 -0500

    Add Select Component

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -1,5 +1,5 @@
-import chroma from "chroma-js";
-const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+import chroma from 'chroma-js';
+const levels = [ 50, 100, 200, 300, 400, 500, 600, 700, 800, 900 ];
 
 function generatePalette(starterPalette) {
 	let newPalette = {
@@ -16,28 +16,22 @@ function generatePalette(starterPalette) {
 		for (let i in scale) {
 			newPalette.colors[levels[i]].push({
 				name: `${color.name} ${levels[i]}`,
-				id: color.name.toLowerCase().replace(/ /g, "-"),
+				id: color.name.toLowerCase().replace(/ /g, '-'),
 				hex: scale[i],
 				rgb: chroma(scale[i]).css(),
-				rgba: chroma(scale[i])
-					.css()
-					.replace("rgb", "rgba")
-					.replace(")", ",1.0)")
+				rgba: chroma(scale[i]).css().replace('rgb', 'rgba').replace(')', ',1.0)')
 			});
 		}
 	}
 	return newPalette;
 }
 function getRange(hexColor) {
-	const end = "#fff";
-	return [chroma(hexColor).darken(1.4).hex(), hexColor, end];
+	const end = '#fff';
+	return [ chroma(hexColor).darken(1.4).hex(), hexColor, end ];
 }
 
 function getScale(hexColor, numberOfColors) {
-	return chroma
-		.scale(getRange(hexColor))
-		.mode("lab")
-		.colors(numberOfColors);
+	return chroma.scale(getRange(hexColor)).mode('lab').colors(numberOfColors);
 }
 
 export { generatePalette };
