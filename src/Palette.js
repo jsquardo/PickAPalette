74215faa36ec903beb06d5d91b827c646ce09676
Johnny Squardo <squ@rdo.host>
2019-12-01 22:18:45 -0500

    Emoji added

--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import ColorBox from './ColorBox';
-import Navbar from './Navbar';
-import './Palette.css';
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import "./Palette.css";
 
 class Palette extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { level: 500, format: 'hex' };
+		this.state = { level: 500, format: "hex" };
 		this.changeLevel = this.changeLevel.bind(this);
 		this.changeFormat = this.changeFormat.bind(this);
 	}
@@ -20,21 +20,23 @@ class Palette extends Component {
 	}
 
 	render() {
-		const { colors } = this.props.palette;
+		const { colors, paletteName, emoji } = this.props.palette;
 		const { level, format } = this.state;
 		const colorBoxes = colors[level].map(color => (
-			<ColorBox background={color[format]} name={color.name} />
+			<ColorBox background={color[format]} name={color.name} key={color.id} />
 		));
 		return (
-			<div className="Palette">
+			<div className='Palette'>
 				<Navbar
 					level={level}
 					changeLevel={this.changeLevel}
 					handleChange={this.changeFormat}
 				/>
-				{/* Navbar goes here */}
-				<div className="Palette-colors">{colorBoxes}</div>
-				{/* footer eventually */}
+				<div className='Palette-colors'>{colorBoxes}</div>
+				<footer className='Palette-footer'>
+					{paletteName}
+					<span className='emoji'>{emoji}</span>
+				</footer>
 			</div>
 		);
 	}
