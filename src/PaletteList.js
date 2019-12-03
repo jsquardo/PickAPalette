ea8b9914d605e559b18edd2ed56b7fe10c19982e
Johnny Squardo <squ@rdo.host>
2019-12-03 09:39:08 -0500

    Make text color dynamic

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,7 +5,7 @@ import { withStyles } from '@material-ui/styles';
 
 const styles = {
 	root: {
-		backgroundColor: 'blue',
+		backgroundColor: 'navy',
 		height: '100%',
 		display: 'flex',
 		alignItems: 'flex-start',
