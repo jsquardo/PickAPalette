d658cb0354d9a7b8d669e5defbf2dcfc20978db7
Johnny Squardo <johnnysquardo@gmail.com>
2019-12-04 17:02:37 -0500

    Refactored Styles into own files

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,40 +2,7 @@ import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
-import background from './Assets/ahoy.jpg';
-
-const styles = {
-	root: {
-		background: `url(${background})`,
-		height: '100vh',
-		width: '100vw',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	container: {
-		width: '50%',
-		display: 'flex',
-		alignItems: 'flex-start',
-		flexDirection: 'column',
-		flexWrap: 'wrap',
-		marginBottom: '3rem'
-	},
-	nav: {
-		display: 'flex',
-		width: '100%',
-		justifyContent: 'space-between',
-		color: 'black',
-		fontSize: '1.25rem'
-	},
-	palettes: {
-		boxSizing: 'border-box',
-		width: '100%',
-		display: 'grid',
-		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '5%'
-	}
-};
+import styles from './Styles/PaletteListStyles';
 
 class PaletteList extends Component {
 	goToPalette(id) {
---
+++ src/Styles/PaletteListStyles.js
@@ -0,0 +1,34 @@
+import background from '../Assets/palettepattern.png';
+
+export default {
+	root: {
+		background: `url(${background})`,
+		height: '100vh',
+		width: '100vw',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	container: {
+		width: '50%',
+		display: 'flex',
+		alignItems: 'flex-start',
+		flexDirection: 'column',
+		flexWrap: 'wrap',
+		marginBottom: '3rem'
+	},
+	nav: {
+		display: 'flex',
+		width: '100%',
+		justifyContent: 'space-between',
+		color: 'white',
+		fontSize: '1.25rem'
+	},
+	palettes: {
+		boxSizing: 'border-box',
+		width: '100%',
+		display: 'grid',
+		gridTemplateColumns: 'repeat(3, 30%)',
+		gridGap: '5%'
+	}
+};
