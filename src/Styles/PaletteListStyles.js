b7a6bc2fb4e291e2d0429f252f10245db67f86c5
Johnny Squardo <johnnysquardo@gmail.com>
2020-01-07 17:13:05 -0500

    Adding Styles

--- src/Styles/PaletteListStyles.js
+++ src/Styles/PaletteListStyles.js
@@ -1,64 +1,67 @@
-
 import sizes from "./sizes";
-import bg from "./bg.svg";
+import bg from "../Assets/palettepattern.png";
 export default {
-  "@global": {
-    ".fade-exit": {
-      opacity: 1
-    },
-    ".fade-exit-active": {
-      opacity: 0,
-      transition: "opacity 500ms ease-out"
-    }
-  },
-  root: {
-    height: "100vh",
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "center",
-    /* background by SVGBackgrounds.com */
-    backgroundColor: "#394bad",
-    backgroundImage: `url(${bg})`,
-    overflow: "scroll"
-  },
-  heading: {
-    fontSize: "2rem"
-  },
-  container: {
-    width: "50%",
-    display: "flex",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    [sizes.down("xl")]: {
-      width: "80%"
-    },
-    [sizes.down("xs")]: {
-      width: "75%"
-    }
-  },
-  nav: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
-    color: "white",
-    "& a": {
-      color: "white"
-    }
-  },
-  palettes: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "2.5rem",
-    [sizes.down("md")]: {
-      gridTemplateColumns: "repeat(2, 50%)"
-    },
-    [sizes.down("xs")]: {
-      gridTemplateColumns: "repeat(1, 100%)",
-      gridGap: "1.4rem"
-    }
-  }
+	"@global": {
+		".fade-exit": {
+			opacity: 1
+		},
+		".fade-exit-active": {
+			opacity: 0,
+			transition: "opacity 500ms ease-out"
+		}
+	},
+	root: {
+		height: "100vh",
+		display: "flex",
+		alignItems: "flex-start",
+		justifyContent: "center",
+		/* background by SVGBackgrounds.com */
+		backgroundColor: "#383c4a",
+		// backgroundImage: `url(${bg})`,
+		overflow: "hidden"
+	},
+	heading: {
+		fontSize: "2rem"
+	},
+	container: {
+		width: "50%",
+		display: "flex",
+		alignItems: "flex-start",
+		flexDirection: "column",
+		flexWrap: "wrap",
+		[sizes.down("xl")]: {
+			width: "80%"
+		},
+		[sizes.down("xs")]: {
+			width: "75%"
+		}
+	},
+	nav: {
+		display: "flex",
+		width: "100%",
+		justifyContent: "space-between",
+		alignItems: "center",
+		color: "white",
+		"& a": {
+			color: "#5294e2",
+			fontSize: "1.25rem",
+			fontWeight: "medium",
+			textDecoration: "none",
+			marginRight: "2.5%"
+		}
+	},
+	palettes: {
+		boxSizing: "border-box",
+		width: "100%",
+		display: "grid",
+		gridTemplateColumns: "repeat(3, 30%)",
+		gridGap: "2.5rem",
+		[sizes.down("md")]: {
+			gridTemplateColumns: "repeat(2, 50%)"
+		},
+		[sizes.down("xs")]: {
+			gridTemplateColumns: "repeat(1, 100%)",
+			gridGap: "1.4rem"
+		}
+	}
 };
